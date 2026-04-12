c6e0bdf4f1199555d04f933a837b79bbee125578
Ayman Abubaker <aymanjaballa@gmail.com>
2026-04-12 20:15:09 +0200

    Update prisma.extension.ts 01c

--- server/src/prisma/prisma.extension.ts
+++ server/src/prisma/prisma.extension.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 import { encrypt, decrypt } from '../common/utils/encryption.util';
 import { createHash } from 'crypto';
 
